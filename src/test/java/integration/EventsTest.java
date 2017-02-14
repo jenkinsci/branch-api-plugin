57b431bb32aad96f2c6816ec65e97f10470669cf
Evaristo Gutiérrez <egutierrez@cloudbees.com>
2017-02-14 15:04:36 +0100

    [JENKINS-42009] Group conditions through allOf.

--- src/test/java/integration/EventsTest.java
+++ src/test/java/integration/EventsTest.java
@@ -883,9 +883,9 @@ public class EventsTest {
             prj.scheduleBuild2(0).getFuture().get();
             r.waitUntilNoActivity();
             assertThat("Indexing applies dead branch cleanup",
-                    prj.getItems(), containsInAnyOrder(master));
-            assertThat("Indexing applies dead branch cleanup",
-                    prj.getItems(), not(containsInAnyOrder(feature)));
+                    prj.getItems(), allOf(
+                    containsInAnyOrder(master),
+                    not(containsInAnyOrder(feature))));
         }
     }
 
