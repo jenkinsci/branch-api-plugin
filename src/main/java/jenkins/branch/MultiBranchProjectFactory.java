b24058cb0fc5a15510575854d105d59e4769f007
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-02-20 11:38:08 +0000

    Javadocs FTW

--- src/main/java/jenkins/branch/MultiBranchProjectFactory.java
+++ src/main/java/jenkins/branch/MultiBranchProjectFactory.java
@@ -217,6 +217,8 @@ public abstract class MultiBranchProjectFactory extends AbstractDescribableImpl<
          * {@link jenkins.scm.api.SCMSourceObserver.ProjectObserver#addAttribute}
          * @return a new uninitialized multibranch project (do not configure its
          * {@link MultiBranchProject#getSourcesList} or call {@link MultiBranchProject#onCreatedFromScratch})
+         * @throws InterruptedException if interrupted.
+         * @throws IOException if there was an IO error.
          */
         @NonNull
         protected abstract MultiBranchProject<?, ?> doCreateProject(@NonNull ItemGroup<?> parent, @NonNull String name,
