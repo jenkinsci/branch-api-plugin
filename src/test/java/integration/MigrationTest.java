014646f1fa571ae4d32953c388f80424dde02f15
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-07-04 14:48:38 +0100

    I think this fixes the tests on windows

--- src/test/java/integration/MigrationTest.java
+++ src/test/java/integration/MigrationTest.java
@@ -318,6 +318,11 @@ public class MigrationTest {
         String koreaMangled = "0_b3_00_d5_5c_bb_fc_ad_6d.ufdgbs";
         String korea2 = "특색/새로운";
         String korea2Mangled = "0_d2_b9_c0_c.ps50ht._b8_5c_c6_b4";
+        // Assume not windows-1252
+        String russia = "Россия";
+        String russiaMangled = "0_04_20_04_3.pei3d7._04_38_04_4f";
+        String china = "中国";
+        String chinaMangled = "0_4e_2d_56_fd.m4k0dn";
         for (MultiBranchProject<?, ?> p : prj.getItems()) {
             String dirName = p.getRootDir().getName();
             System.out.printf("%s ==> %s ==> %s == \"%s\"%n", dirName, p.getName(), p.getDisplayName(), asJavaString(p.getDisplayName()));
@@ -329,7 +334,7 @@ public class MigrationTest {
             }
             if (dirName.equals("Espan_cc_01_92a.po41g5")) {
                 // Windows-1252
-                espana = "EspanÌƒa";
+                espana = "Espan\u00cc\u0192a";
                 espanaMangled = "Espan_cc_01_92a.po41g5";
             }
             if (dirName.equals("E_03_01ireann.0qtq11")) {
@@ -339,8 +344,18 @@ public class MigrationTest {
             }
             if (dirName.equals("E_cc_ff_fdireann.k4nq8h")) {
                 // Windows-1252
-                ireland = "EÌ\ufffdireann";
+                ireland = "E\u00cc\ufffdireann";
                 irelandMangled = "E_cc_ff_fdireann.k4nq8h";
+            }
+            if (dirName.equals("0_d0_a0_d.co5tjl6h6dbk._d1_ff_fd")) {
+                // Windows-1252
+                russia = "\u00d0\u00a0\u00d0\u00be\u00d1\ufffd\u00d1\ufffd\u00d0\u00b8\u00d1\ufffd";
+                russiaMangled = "0_d0_a0_d.co5tjl6h6dbk._d1_ff_fd";
+            }
+            if (dirName.equals("0_e4_b8_ad_e5_20_3a_bd.546c3v")) {
+                // Windows-1252
+                china = "\u00e4\u00b8\u00ad\u00e5\u203a\u00bd";
+                chinaMangled = "0_e4_b8_ad_e5_20_3a_bd.546c3v";
             }
             if (dirName.equals("0_11_03_1.3gi5g2rs7pg4._6e_11_a8")) {
                 // NFD
@@ -349,7 +364,9 @@ public class MigrationTest {
             }
             if (dirName.equals("0_e1_20_1.78185nsomvje._20_20_a8")) {
                 // Windows-1252
-                korea = "á„ƒá…¢á„’á…¡á†«á„†á…µá†«á„€á…®á†¨";
+                korea = "\u00e1\u201e\u0192\u00e1\u2026\u00a2\u00e1\u201e\u2019\u00e1\u2026\u00a1\u00e1\u2020\u00ab"
+                        + "\u00e1\u201e\u2020\u00e1\u2026\u00b5\u00e1\u2020\u00ab\u00e1\u201e\u20ac\u00e1\u2026\u00ae"
+                        + "\u00e1\u2020\u00a8";
                 koreaMangled = "0_e1_20_1.78185nsomvje._20_20_a8";
             }
             byDirName.put(dirName, p);
@@ -362,11 +379,6 @@ public class MigrationTest {
                     // NFD
                     korea2 = "\u1110\u1173\u11a8\u1109\u1162\u11a8/\u1109\u1162\u1105\u1169\u110b\u116e\u11ab";
                     korea2Mangled = "0_11_10_1.m479ph0h00p7._6e_11_ab";
-                }
-                if (j.getName().equals("0_d2_b9_c0_c.ps50ht._b8_5c_c6_b4")) {
-                    // Windows-1252
-                    korea2 = "\u1110\u1173\u11a8\u1109\u1162\u11a8/\u1109\u1162\u1105\u1169\u110b\u116e\u11ab";
-                    korea2Mangled = "0_d2_b9_c0_c.ps50ht._b8_5c_c6_b4";
                 }
                 jobByName.put(j.getFullName(), j);
                 jobByDirName.put(jobDirName, j);
@@ -382,24 +394,24 @@ public class MigrationTest {
         assertThat("Display Names are repo names", byDisplayName.keySet(), containsInAnyOrder(
                 "test.example.com",
                 ireland,
-                "Россия",
-                "中国",
+                russia,
+                china,
                 espana,
                 korea
         ));
         assertThat("Directory names have been mangled", byDirName.keySet(), containsInAnyOrder(
                 "test-example-com.34nhgh",
                 irelandMangled,  // Éireann
-                "0_04_20_04_3.pei3d7._04_38_04_4f", // Россия
-                "0_4e_2d_56_fd.m4k0dn", // 中国
+                russiaMangled, // Россия
+                chinaMangled, // 中国
                 espanaMangled, // España
                 koreaMangled // 대한민국
         ));
         assertThat("Folder names have been minimal url path segment pre-encoded", byName.keySet(), containsInAnyOrder(
                 "test.example.com",
                 ireland,  // Éireann
-                "Россия", // Россия
-                "中国", // 中国
+                russia, // Россия
+                china, // 中国
                 espana, // España
                 korea // 대한민국
         ));
@@ -411,10 +423,10 @@ public class MigrationTest {
                 "foo » test.example.com » feature: welcome",
                 "foo » " + ireland + " » master",
                 "foo » " + ireland + " » gné/nua",
-                "foo » Россия » master",
-                "foo » Россия » особенность/новый",
-                "foo » 中国 » master",
-                "foo » 中国 » 特征/新",
+                "foo » " + russia + " » master",
+                "foo » " + russia + " » особенность/новый",
+                "foo » " + china + " » master",
+                "foo » " + china + " » 特征/新",
                 "foo » " + espana + " » master",
                 "foo » " + espana + " » característica/nuevo",
                 "foo » " + korea + " » master",
@@ -427,10 +439,10 @@ public class MigrationTest {
                 "foo/test-example-com.34nhgh/feature_3a-welcome.9dhrtb",
                 "foo/" + irelandMangled + "/master",
                 "foo/" + irelandMangled + "/gn_e9-nua.updi5h",
-                "foo/0_04_20_04_3.pei3d7._04_38_04_4f/master",
-                "foo/0_04_20_04_3.pei3d7._04_38_04_4f/0_04_3e_0.n168ksdsksof._4b_04_39",
-                "foo/0_4e_2d_56_fd.m4k0dn/master",
-                "foo/0_4e_2d_56_fd.m4k0dn/0_72_79_5f_81-_65_b0.nt1m48",
+                "foo/" + russiaMangled + "/master",
+                "foo/" + russiaMangled + "/0_04_3e_0.n168ksdsksof._4b_04_39",
+                "foo/" + chinaMangled + "/master",
+                "foo/" + chinaMangled + "/0_72_79_5f_81-_65_b0.nt1m48",
                 "foo/" + espanaMangled + "/master",
                 "foo/" + espanaMangled + "/caracter_edstica-nuevo.h5da9f",
                 "foo/" + koreaMangled + "/master",
@@ -444,10 +456,10 @@ public class MigrationTest {
                 "foo/test.example.com/feature: welcome",
                 "foo/" + ireland + "/master",
                 "foo/" + ireland + "/gné%2Fnua",
-                "foo/Россия/master",
-                "foo/Россия/особенность%2Fновый",
-                "foo/中国/master",
-                "foo/中国/特征%2F新",
+                "foo/" + russia + "/master",
+                "foo/" + russia + "/особенность%2Fновый",
+                "foo/" + china + "/master",
+                "foo/" + china + "/特征%2F新",
                 "foo/" + espana + "/master",
                 "foo/" + espana + "/característica%2Fnuevo",
                 "foo/" + korea + "/master",
