77e5867424d943fdfaed0958ac6bdcbd84c791c7
Stephen Connolly <stephen.alan.connolly@gmail.com>
2019-11-05 10:39:25 +0000

    [JENKINS-59786] Add clarifying method extraction to remove the noise resulting from avoiding generics type erasure

--- src/test/java/jenkins/branch/PrimaryBranchHealthMetricTest.java
+++ src/test/java/jenkins/branch/PrimaryBranchHealthMetricTest.java
@@ -2,13 +2,16 @@ package jenkins.branch;
 
 import hudson.model.FreeStyleProject;
 import hudson.model.TopLevelItem;
-import integration.harness.BasicMultiBranchProject;
 import integration.harness.HealthReportingMultiBranchProject;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import jenkins.scm.impl.mock.MockSCMController;
 import jenkins.scm.impl.mock.MockSCMDiscoverBranches;
 import jenkins.scm.impl.mock.MockSCMSource;
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -64,9 +67,10 @@ public class PrimaryBranchHealthMetricTest {
             prj.invalidateBuildHealthReports();
             FreeStyleProject master = prj.getItem("master");
             assertThat("We now have the master branch", master, notNullValue());
-            assertThat(prj.getBuildHealthReports(), containsInAnyOrder(
-                    master.getBuildHealthReports().stream().map(Matchers::is).collect(Collectors.toList())
-            ));
+            assertThat("'master' branch's reports should be exactly the reports as it is the primary branch",
+                    prj.getBuildHealthReports(),
+                    containsInAnyOrder(asMatchers(master.getBuildHealthReports(), Matchers::is))
+            );
         }
     }
 
@@ -75,6 +79,8 @@ public class PrimaryBranchHealthMetricTest {
         try (MockSCMController c = MockSCMController.create()) {
             c.createRepository("foo");
             c.createBranch("foo", "stable");
+            // we want to ensure that the implementation is based on the SCM's reported primary branch and not just
+            // hard-coded to the string 'master'
             c.setPrimaryBranch("foo", "stable");
             HealthReportingMultiBranchProject prj = r.jenkins.createProject(HealthReportingMultiBranchProject.class, "foo");
             prj.setCriteria(null);
@@ -87,13 +93,35 @@ public class PrimaryBranchHealthMetricTest {
             assertThat("We now have the master branch", master, notNullValue());
             FreeStyleProject stable = prj.getItem("stable");
             assertThat("We now have the stable branch", stable, notNullValue());
-            assertThat(prj.getBuildHealthReports(), not(containsInAnyOrder(
-                    master.getBuildHealthReports().stream().map(Matchers::is).collect(Collectors.toList())
-            )));
-            assertThat(prj.getBuildHealthReports(), containsInAnyOrder(
-                    stable.getBuildHealthReports().stream().map(Matchers::is).collect(Collectors.toList())
-            ));
+            assertThat(
+                    "'master' branch's reports should not be present as it is not the primary branch",
+                    prj.getBuildHealthReports(),
+                    not(
+                            containsInAnyOrder(asMatchers(master.getBuildHealthReports(), Matchers::is))
+                    )
+            );
+            assertThat(
+                    "'stable' branch's reports should be exactly the reports as it is the primary branch",
+                    prj.getBuildHealthReports(),
+                    containsInAnyOrder(asMatchers(stable.getBuildHealthReports(), Matchers::is))
+            );
         }
+    }
+
+    /**
+     * Helper for matching a list of instances with {@link Matchers#containsInAnyOrder(Collection)} or {@link
+     * Matchers#contains(List)} as these expect a list of {@link Matcher} instance not a list of instances so you will
+     * get a test failure due to a class cast exception because of type erasure.
+     *
+     * @param instances the instances you want to match.
+     * @param matcherFunction the function to use for converting instances to matchers, normally {@link
+     *         Matchers#is(Object)}.
+     * @param <T> the type of instance.
+     * @param <V> the type of object matched by the resulting matcher.
+     * @return a list of {@link Matcher} instance, one for every instance in the input list.
+     */
+    private static <T,V> List<Matcher<V>> asMatchers(List<T> instances, Function<T, Matcher<V>> matcherFunction) {
+        return instances.stream().map(matcherFunction).collect(Collectors.toList());
     }
 
 
