529da398c6a93a45f99e9704b972a8bf41f3c7f8
Nikolas Falco <nfalco79@hotmail.com>
2020-05-05 19:26:02 +0200

    Fix mockito issues for BranchCategoryFilterTest

--- src/test/java/jenkins/branch/BranchCategoryFilterTest.java
+++ src/test/java/jenkins/branch/BranchCategoryFilterTest.java
@@ -29,16 +29,26 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.model.ItemGroup;
 import hudson.model.Job;
 import hudson.model.Run;
+import hudson.model.TaskListener;
 import hudson.model.TopLevelItem;
+import hudson.model.TopLevelItemDescriptor;
 import hudson.model.View;
 import hudson.scm.SCM;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.SortedMap;
+
 import javax.annotation.Nonnull;
+
+import jenkins.scm.api.SCMHead;
 import jenkins.scm.api.SCMHeadCategory;
+import jenkins.scm.api.SCMHeadEvent;
+import jenkins.scm.api.SCMHeadObserver;
+import jenkins.scm.api.SCMRevision;
 import jenkins.scm.api.SCMSource;
+import jenkins.scm.api.SCMSourceCriteria;
 import jenkins.scm.api.SCMSourceDescriptor;
 import jenkins.scm.impl.ChangeRequestSCMHeadCategory;
 import jenkins.scm.impl.TagSCMHeadCategory;
@@ -49,7 +59,6 @@ import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -61,28 +70,25 @@ public class BranchCategoryFilterTest {
         when(filteringView.getOwner()).thenReturn(owner);
         BranchProjectFactory factory = mock(BranchProjectFactory.class);
         when(owner.getProjectFactory()).thenReturn(factory);
-        MockSCMSource source = mock(MockSCMSource.class);
-        SCMSourceDescriptor sourceDescriptor = new MockSCMSource.MockSCMSourceDescriptor();
+        MockSCMSource source = new MockSCMSource();
         when(owner.getSCMSources()).thenReturn(Collections.singletonList(source));
-        when(source.getDescriptor()).thenReturn(sourceDescriptor);
-        when(source.isCategoryEnabled(any(SCMHeadCategory.class))).thenReturn(true);
-        TopLevelJob child1 = mock(TopLevelJob.class);
+        TopLevelJob child1 = new TopLevelJob("child1");
         when(factory.isProject(child1)).thenReturn(true);
         when(factory.asProject(child1)).thenReturn(child1);
         SCM scm = mock(SCM.class);
         when(factory.getBranch(child1)).thenReturn(
                 new Branch("1", new MockSCMHead("master"), scm, Collections.<BranchProperty>emptyList()));
-        TopLevelJob child2 = mock(TopLevelJob.class);
+        TopLevelJob child2 = new TopLevelJob("child2");
         when(factory.isProject(child2)).thenReturn(true);
         when(factory.asProject(child2)).thenReturn(child2);
         when(factory.getBranch(child2)).thenReturn(new Branch("1", new MockSCMHead("fork"), scm,
                 Collections.<BranchProperty>emptyList()));
-        TopLevelJob child3 = mock(TopLevelJob.class);
+        TopLevelJob child3 = new TopLevelJob("child3");
         when(factory.isProject(child3)).thenReturn(true);
         when(factory.asProject(child3)).thenReturn(child3);
         when(factory.getBranch(child3)).thenReturn(new Branch("1", new MockTagSCMHead("master-1.0", 0L), scm,
                 Collections.<BranchProperty>emptyList()));
-        TopLevelJob child4 = mock(TopLevelJob.class);
+        TopLevelJob child4 = new TopLevelJob("child4");
         when(factory.isProject(child4)).thenReturn(false);
         List<TopLevelItem> added = new ArrayList<>();
         added.add(child1);
@@ -112,28 +118,25 @@ public class BranchCategoryFilterTest {
         when(filteringView.getOwner()).thenReturn(owner);
         BranchProjectFactory factory = mock(BranchProjectFactory.class);
         when(owner.getProjectFactory()).thenReturn(factory);
-        MockSCMSource source = mock(MockSCMSource.class);
-        SCMSourceDescriptor sourceDescriptor = new MockSCMSource.MockSCMSourceDescriptor();
+        MockSCMSource source = new MockSCMSource();
         when(owner.getSCMSources()).thenReturn(Collections.singletonList(source));
-        when(source.getDescriptor()).thenReturn(sourceDescriptor);
-        when(source.isCategoryEnabled(any(SCMHeadCategory.class))).thenReturn(true);
-        TopLevelJob child1 = mock(TopLevelJob.class);
+        TopLevelJob child1 = new TopLevelJob("child1");
         when(factory.isProject(child1)).thenReturn(true);
         when(factory.asProject(child1)).thenReturn(child1);
         SCM scm = mock(SCM.class);
         when(factory.getBranch(child1)).thenReturn(
                 new Branch("1", new MockSCMHead("master"), scm, Collections.<BranchProperty>emptyList()));
-        TopLevelJob child2 = mock(TopLevelJob.class);
+        TopLevelJob child2 = new TopLevelJob("child2");
         when(factory.isProject(child2)).thenReturn(true);
         when(factory.asProject(child2)).thenReturn(child2);
         when(factory.getBranch(child2)).thenReturn(new Branch("1", new MockSCMHead("fork"), scm,
                 Collections.<BranchProperty>emptyList()));
-        TopLevelJob child3 = mock(TopLevelJob.class);
+        TopLevelJob child3 = new TopLevelJob("child3");
         when(factory.isProject(child3)).thenReturn(true);
         when(factory.asProject(child3)).thenReturn(child3);
         when(factory.getBranch(child3)).thenReturn(new Branch("1", new MockTagSCMHead("master-1.0", 0L), scm,
                 Collections.<BranchProperty>emptyList()));
-        TopLevelJob child4 = mock(TopLevelJob.class);
+        TopLevelJob child4 = new TopLevelJob("child4");
         when(factory.isProject(child4)).thenReturn(false);
         List<TopLevelItem> added = new ArrayList<>();
         added.add(child1);
@@ -147,10 +150,29 @@ public class BranchCategoryFilterTest {
 
     }
 
-    private static abstract class TopLevelJob extends Job<TopLevelJob, TopLevelRun> implements TopLevelItem {
+    private static class TopLevelJob extends Job<TopLevelJob, TopLevelRun> implements TopLevelItem {
 
-        protected TopLevelJob(ItemGroup parent, String name) {
-            super(parent, name);
+        protected TopLevelJob(String name) {
+            super(null, name);
+        }
+
+        @Override
+        public TopLevelItemDescriptor getDescriptor() {
+            return null;
+        }
+
+        @Override
+        public boolean isBuildable() {
+            return false;
+        }
+
+        @Override
+        protected SortedMap<Integer, ? extends TopLevelRun> _getRuns() {
+            return null;
+        }
+
+        @Override
+        protected void removeRun(TopLevelRun run) {
         }
     }
 
@@ -161,15 +183,23 @@ public class BranchCategoryFilterTest {
         }
     }
 
-    private static abstract class MockSCMSource extends SCMSource {
+    private static class MockSCMSource extends SCMSource {
+
+        private MockSCMSourceDescriptor descriptor;
 
         protected MockSCMSource() {
             super("1");
+            this.descriptor = new MockSCMSourceDescriptor();
         }
 
         @Override
         public boolean isCategoryEnabled(@NonNull SCMHeadCategory category) {
             return true;
+        }
+
+        @Override
+        public SCMSourceDescriptor getDescriptor() {
+            return descriptor;
         }
 
         private static class MockSCMSourceDescriptor extends SCMSourceDescriptor {
@@ -183,6 +213,18 @@ public class BranchCategoryFilterTest {
                         ChangeRequestSCMHeadCategory.DEFAULT
                 };
             }
+        }
+
+        @Override
+        protected void retrieve(SCMSourceCriteria criteria,
+                                SCMHeadObserver observer,
+                                SCMHeadEvent<?> event,
+                                TaskListener listener) throws IOException, InterruptedException {
+        }
+
+        @Override
+        public SCM build(SCMHead head, SCMRevision revision) {
+            return null;
         }
 
     }
