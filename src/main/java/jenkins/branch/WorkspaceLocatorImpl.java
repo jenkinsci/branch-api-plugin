203f69c01bc00602b83b34b3cbbbb0259c02c864
Thierry Lacour <tlacour@verifa.io>
2022-11-29 13:49:14 +0100

    Remove unused import

--- src/main/java/jenkins/branch/WorkspaceLocatorImpl.java
+++ src/main/java/jenkins/branch/WorkspaceLocatorImpl.java
@@ -50,7 +50,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.lang.management.ManagementFactory;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
